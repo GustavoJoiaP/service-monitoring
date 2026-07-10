c38f4df7dba13155c1607e73ac55e4bb6d30f0c3
Gustavo Joia Paziani <joia.gustavo@gmail.com>
2026-07-10 15:35:01 -0300

    refactor podman file

--- scripts/ports_check.py
+++ scripts/ports_check.py
@@ -12,7 +12,7 @@ from copy import deepcopy
 
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
 COMPOSE_FILE = PROJECT_ROOT / "podman" / "podman-compose.yaml"
 
